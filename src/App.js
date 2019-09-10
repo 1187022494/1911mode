711477bbfc4a531dda7ca455eb355607abb43086
1187022494 <1187022494@qq.com>
2019-09-10 22:59:01 +0800

    adminit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './test.less';
-
+import 'style/test.less';
+import {Button} from 'antd'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button type="danger">hehe</Button>
         <h3>你好，WORID</h3>
       </header>
     </div>
